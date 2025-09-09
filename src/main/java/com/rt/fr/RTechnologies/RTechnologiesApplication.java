301c94fb1c857621bfe3f8ba7e4278a4c6d75961
Walid Lfaleh <lfalehwalid@gmail.com>
2025-09-09 10:20:24 +0100

    init backend java

---
+++ src/main/java/com/rt/fr/RTechnologies/RTechnologiesApplication.java
@@ -0,0 +1,13 @@
+package com.rt.fr.RTechnologies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RTechnologiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RTechnologiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rt/fr/RTechnologies/RTechnologiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rt.fr.RTechnologies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RTechnologiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
